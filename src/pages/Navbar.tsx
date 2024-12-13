ac09a0e22a876222977e722ccfc1dcc9033517b3
DhruvGaur32 <154977139+DhruvGaur32@users.noreply.github.com>
2024-12-13 13:47:18 +0530

    Navbar mobile view updated

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -5,49 +5,23 @@ function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-primary fixed top-4 left-1/2 transform -translate-x-1/2 w-[95%] lg:w-4/5 z-50 shadow-xl rounded-full">
+    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 w-[95%] lg:w-4/5 z-50 w-full z-50 bg-primary shadow-lg rounded-full">
       <nav className="text-white py-4">
         <div className="max-w-7xl mx-auto flex items-center justify-between px-4 md:px-6 lg:px-8">
           {/* Logo */}
           <div className="text-2xl font-bold tracking-wide">
             <h1 className="text-2xl lg:text-3xl tracking-tight">
-              <span>Sustain·</span>
-              <span className="font-semibold text-textHighlight">ability</span>
+              <span>Sustain·</span><span className="font-semibold text-textHighlight">ability</span>
             </h1>
           </div>
 
           {/* Menu for larger screens */}
           <div className="hidden md:flex space-x-6 text-lg font-playfair">
-            <Link
-              to="/"
-              className="hover:text-gray-400 transition duration-300 ease-in-out"
-            >
-              Home
-            </Link>
-            <Link
-              to="/services"
-              className="hover:text-gray-400 transition duration-300 ease-in-out"
-            >
-              Services
-            </Link>
-            <Link
-              to="/portfolio"
-              className="hover:text-gray-400 transition duration-300 ease-in-out"
-            >
-              Portfolio
-            </Link>
-            <Link
-              to="/about-us"
-              className="hover:text-gray-400 transition duration-300 ease-in-out"
-            >
-              About Us
-            </Link>
-            <Link
-              to="/contact-us"
-              className="hover:text-gray-400 transition duration-300 ease-in-out"
-            >
-              Contact Us
-            </Link>
+            <Link to="/" className="hover:text-gray-400 transition duration-300 ease-in-out">Home</Link>
+            <Link to="/services" className="hover:text-gray-400 transition duration-300 ease-in-out">Services</Link>
+            <Link to="/portfolio" className="hover:text-gray-400 transition duration-300 ease-in-out">Portfolio</Link>
+            <Link to="/about-us" className="hover:text-gray-400 transition duration-300 ease-in-out">About Us</Link>
+            <Link to="/contact-us" className="hover:text-gray-400 transition duration-300 ease-in-out">Contact Us</Link>
           </div>
 
           {/* Hamburger Menu for smaller screens */}
@@ -65,11 +39,7 @@ function Navbar() {
                 stroke="currentColor"
                 className="w-6 h-6"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
           </div>
@@ -77,9 +47,7 @@ function Navbar() {
 
         {/* Mobile Menu */}
         {isMobileMenuOpen && (
-          <div
-            className="fixed inset-0 bg-gradient-to-br from-baseGreen via-primary to-green-800 text-white flex flex-col items-center justify-center z-50 transition-transform duration-500 rounded-full"
-          >
+          <div className="fixed h-screen inset-0 bg-gradient-to-br from-baseGreen via-primary to-green-800 text-white flex flex-col items-center justify-center z-50 rounded-lg transition-transform duration-500">
             <button
               type="button"
               className="absolute top-5 right-5 text-white hover:text-green-400 focus:outline-none transition duration-300"
@@ -93,52 +61,18 @@ function Navbar() {
                 stroke="currentColor"
                 className="w-8 h-8"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
 
-            <div className="flex flex-col items-center space-y-6 font-playfair text-lg">
-              <Link
-                to="/"
-                className="hover:text-green-400 transition duration-300 ease-in-out text-2xl"
-                onClick={() => setIsMobileMenuOpen(false)}
-              >
-                Home
-              </Link>
-              <Link
-                to="/services"
-                className="hover:text-green-400 transition duration-300 ease-in-out text-2xl"
-                onClick={() => setIsMobileMenuOpen(false)}
-              >
-                Services
-              </Link>
-              <Link
-                to="/portfolio"
-                className="hover:text-green-400 transition duration-300 ease-in-out text-2xl"
-                onClick={() => setIsMobileMenuOpen(false)}
-              >
-                Portfolio
-              </Link>
-              <Link
-                to="/about-us"
-                className="hover:text-green-400 transition duration-300 ease-in-out text-2xl"
-                onClick={() => setIsMobileMenuOpen(false)}
-              >
-                About Us
-              </Link>
-              <Link
-                to="/contact-us"
-                className="hover:text-green-400 transition duration-300 ease-in-out text-2xl"
-                onClick={() => setIsMobileMenuOpen(false)}
-              >
-                Contact Us
-              </Link>
+            <div className="flex flex-col items-center space-y-6 font-playfair text-lg w-full">
+              <Link to="/" className="hover:text-green-400 transition duration-300 ease-in-out text-2xl" onClick={() => setIsMobileMenuOpen(false)}>Home</Link>
+              <Link to="/services" className="hover:text-green-400 transition duration-300 ease-in-out text-2xl" onClick={() => setIsMobileMenuOpen(false)}>Services</Link>
+              <Link to="/portfolio" className="hover:text-green-400 transition duration-300 ease-in-out text-2xl" onClick={() => setIsMobileMenuOpen(false)}>Portfolio</Link>
+              <Link to="/about-us" className="hover:text-green-400 transition duration-300 ease-in-out text-2xl" onClick={() => setIsMobileMenuOpen(false)}>About Us</Link>
+              <Link to="/contact-us" className="hover:text-green-400 transition duration-300 ease-in-out text-2xl" onClick={() => setIsMobileMenuOpen(false)}>Contact Us</Link>
             </div>
-          </div>
+          </ div>
         )}
       </nav>
     </div>
